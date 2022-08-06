a8bb3ec51b4640b8b5b1c2e96593a1d74f64eb1f
swautele <swautle@student.s19.com>
2022-08-06 14:13:32 +0200

    save in progress

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,6 +1,14 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-
+#include <iostream>
+#include <stack>
+template <class T>
+class MutantStack: public std::stack<T>{
+	public:
+	
+	private:
+	protected:
+};
 
 #endif
