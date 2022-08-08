b481f394dafa1344ff1be367c962b5f5d2cf2f77
swautele <swautle@student.s19.com>
2022-08-08 15:41:42 +0200

    save still more test to do

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,15 +5,17 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Last number of stack is " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
+	std::cout << "Size of stack is " << mstack.size() << std::endl;
+	mstack.push(3); mstack.push(5); mstack.push(737);
 	MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite) {
-		std::cout << *it << std::endl;
+		std::cout << "Number of stack is " << *it << std::endl;
 	++it; }
-	std::stack<int> s(mstack); return 0;
+	std::stack<int> s(mstack);
+	s = mstack;
+	return 0;
 }
