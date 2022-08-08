11a013f43cff3d793c892fb379181354f744654e
swautele <swautle@student.s19.com>
2022-08-08 15:51:04 +0200

    save GG

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,11 +9,12 @@ int main()
 	mstack.pop();
 	std::cout << "Size of stack is " << mstack.size() << std::endl;
 	mstack.push(3); mstack.push(5); mstack.push(737);
+	std::cout << "Last number of stack is " << mstack.top() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite) {
-		std::cout << "Number of stack is " << *it << std::endl;
+		std::cout << "Number in the stack is " << *it << std::endl;
 	++it; }
 	std::stack<int> s(mstack);
 	s = mstack;
