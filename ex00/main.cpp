e6d5b6b554a21344c852d4335b61fae1a08cc29a
swautele <swautle@student.s19.com>
2022-08-08 19:58:05 +0200

    fuck

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,16 +7,16 @@ int main()
 	std::srand(time(NULL));
 	int	searched = std::rand() % 10;
 	std::cout << "For this test i'll search the number " << searched << std::endl << std::endl;
-	std::array<int, size> test;
+	std::vector<int> vect;
 	for (unsigned int i = 0; i < size; i++)
 	{
-		test[i] = std::rand() % 10;
-		std::cout << "pos " << i << " initialized with " << test[i] << std::endl;
+		vect.push_back(std::rand() % 10);
+		std::cout << "pos " << i << " initialized with " << vect[i] << std::endl;
 	}
 	try
 	{
-		int res = easyfind(test, searched);
-		std::cout << "Number found in pos " << res << ", it was the number " << searched << std::endl;
+		std::vector<int>::iterator res = easyfind(vect, searched);
+		std::cout << "Number found in pos " << *res << ", it was the number " << searched << std::endl;
 	}
 	catch(const std::exception& e)
 	{
